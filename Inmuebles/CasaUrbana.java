8957c96811c2a3a5d8168f3b75c9efcc3be190c9
manuela-Cha <mchaverraz@unal.edu.co>
2024-08-18 12:54:14 -0500

    Taller_4

---
+++ Inmuebles/CasaUrbana.java
@@ -0,0 +1,16 @@
+package Inmuebles;
+
+public class CasaUrbana extends Casa {
+
+    public CasaUrbana(int identificadorInmobiliario, int área, String
+    dirección, int númeroHabitaciones, int númeroBaños, int
+    númeroPisos) {
+
+        super(identificadorInmobiliario, área, dirección,
+        númeroHabitaciones, númeroBaños, númeroPisos);
+    }
+
+    void imprimir() {
+        super.imprimir(); 
+    }
+}
